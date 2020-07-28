2386027de8277839dbcdffeca2bbf475cf3b9e62
Nicolas <nicolas.rondon@servinforamcion.com>
2020-07-27 20:35:15 -0500

    antes de la migracion

--- core/urls/v1.py
+++ core/urls/v1.py
@@ -3,6 +3,10 @@ from  rest_framework import routers
 from learn.views import LessonViewSet, ChapterViewSet, CourseViewSet
 
 router = routers.DefaultRouter()
+router.register(r'cursos', ChapterViewSet)
+router.register(r'lecciones', LessonViewSet)
+router.register(r'capitulos', CourseViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls))
