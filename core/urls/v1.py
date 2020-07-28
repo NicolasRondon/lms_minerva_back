f89576aed7882461cadd4b0be5b7e45121aed63a
Nicolas <nicolas.rondon@servinforamcion.com>
2020-07-28 17:34:21 -0500

    v1

--- core/urls/v1.py
+++ core/urls/v1.py
@@ -3,9 +3,10 @@ from  rest_framework import routers
 from learn.views import LessonViewSet, ChapterViewSet, CourseViewSet
 
 router = routers.DefaultRouter()
-router.register(r'cursos', ChapterViewSet)
+router.register(r'cursos', CourseViewSet)
 router.register(r'lecciones', LessonViewSet)
-router.register(r'capitulos', CourseViewSet)
+router.register(r'capitulos', ChapterViewSet)
+
 
 
 urlpatterns = [
--- learn/models.py
+++ learn/models.py
@@ -1,35 +1,36 @@
 from django.db import models
 
 # Create your models here.
-class Chapter(models.Model):
+class Course(models.Model):
     title = models.CharField(max_length=500, blank=False)
     body = models.TextField()
     date_created = models.DateTimeField(auto_now=True)
     slug = models.SlugField(allow_unicode=True)
-    video = models.FileField()
 
     def __str__(self):
         return self.title
-
-
 
 class Lesson(models.Model):
     title = models.CharField(max_length=500, blank=False)
     body = models.TextField()
     date_created = models.DateTimeField(auto_now=True)
     slug = models.SlugField(allow_unicode=True)
-    chapters = models.ForeignKey(Chapter, related_name='chapters', on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, related_name='chapters', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
 
-class Course(models.Model):
+class Chapter(models.Model):
     title = models.CharField(max_length=500, blank=False)
     body = models.TextField()
     date_created = models.DateTimeField(auto_now=True)
     slug = models.SlugField(allow_unicode=True)
-    lessons = models.ForeignKey(Lesson, related_name='lessons', on_delete=models.CASCADE)
+    video = models.FileField()
+    lesson= models.ForeignKey(Lesson, related_name='lesson', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+
+
